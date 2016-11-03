13d34c918fc4cd159ec5d7f89bdba2099ee35a57
fran-aubry <fran.aubry@gmail.com>
2016-11-03 19:35:48 +0100

    Add files via upload

---
+++ basic_agent.py
@@ -0,0 +1,34 @@
+
+"""
+Agent skeleton. Fill in the gaps.
+"""
+class MyAgent(Agent):
+
+    """This is the skeleton of an agent to play the Penguin game."""
+    
+    def get_action(self, state, time_left):
+        """This function is used to play a move according
+        to the board, player and time left provided as input.
+        It must return an action representing the move the player
+        will perform.
+        """
+        return minimax.search(state, self)
+
+    def successors(self, state):
+        """The successors function must return (or yield) a list of
+        pairs (a, s) in which a is the action played to reach the
+        state s;
+        """
+        pass
+
+    def cutoff(self, state, depth):
+        """The cutoff function returns true if the alpha-beta/minimax
+        search has to stop; false otherwise.
+        """
+        pass
+
+    def evaluate(self, state):
+        """The evaluate function must return an integer value
+        representing the utility function of the board.
+        """
+        pass
